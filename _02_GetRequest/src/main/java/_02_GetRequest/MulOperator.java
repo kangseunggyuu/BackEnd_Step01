4458ba1a888e42e9b07be4e145e7c463e0ebb533
kangseunggyuu <kscu7310@naver.com>
2024-03-26 18:01:59 +0900

    BackEnd 첫번째 수업

---
+++ _02_GetRequest/src/main/java/_02_GetRequest/MulOperator.java
@@ -0,0 +1,16 @@
+package _02_GetRequest;
+
+public class MulOperator implements Operator {
+
+	@Override
+	public String getName() {
+		return "*";
+	}
+
+	@Override
+	public double execute(double a, double b) throws Exception {
+		// TODO Auto-generated method stub
+		return a * b;
+	}
+
+}
