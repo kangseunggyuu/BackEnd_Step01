0ff1af0eca4dc3c242542ce2d50d723873ca1893
kangseunggyuu <kscu7310@naver.com>
2024-04-02 19:10:06 +0900

    백엔드 세번째 ~ 여섯번째 수업

--- _11_MemberListServlet_Refresh/src/main/java/spms/sevlets/MemberAddServlet.java
+++ _11_MemberListServlet_Refresh/src/main/java/spms/sevlets/MemberAddServlet.java
@@ -73,7 +73,7 @@ public class MemberAddServlet extends HttpServlet{
 			resp.setContentType("text/html;charset=UTF-8");
 			PrintWriter out = resp.getWriter();
 			out.println("<html><head><title>회원등록결과</title></head>");
-			out.println("<meta http-equiv='Refresh' content='1;url=list'>");
+			out.println("<meta http-equiv='Refresh' content='0.5;url=list'>");
 			out.println("<body>");
 			out.println("<p>등록 성공입니다</p>");
 			out.println("</body></html>");
---
+++ _21_JSP/src/main/webapp/Hello.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <!-- [JSP가 JAVA로 변환된 파일 위치]
+    C:\BitCamp\BackEnd_Step01\.metadata\.plugins\org.eclipse.wst.server.core\tmp0\work\Catalina\localhost\_19_Filter\org\apache\jsp -->
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Hello</title>
+</head>
+<body>
+	<p>안녕하세요</p>
+</body>
+</html>
