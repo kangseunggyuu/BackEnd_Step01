61538cecd4bf4d2f3466e2db91878e083ba6fa80
kangseunggyuu <kscu7310@naver.com>
2024-04-03 18:07:42 +0900

    백엔드 여섯번째 수업 및 오라클 첫번째 수업

---
+++ _56_DataBinding/src/main/java/spms/dao/MemberDao.java
@@ -0,0 +1,17 @@
+package spms.dao;
+
+import java.util.List;
+
+import spms.vo.Member;
+
+public interface MemberDao {
+
+	// interface는 상속을 전제로 하므로 자동으로 public 속성이 부여된다.
+	
+	List<Member> selectList() throws Exception;
+	int insert(Member member) throws Exception;
+	int delete(int no) throws Exception;
+	Member selectOne(int no) throws Exception;
+	int update(Member member) throws Exception;
+	Member exist(String email, String password) throws Exception;
+}
